cfdb72d732e5b041ac172521414f2ee92b128b18
Byakko12 <davidlandg@gmail.com>
2022-09-19 22:19:00 -0500

    [FIX] - identation of file

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """
-add - takes a float a and a float b as arguments and returns their sum as a float.
+add - takes a float a and a float b as arguments and
+returns their sum as a float.
 """
-def add (a: float, b: float) -> float:
+
+
+def add(a: float, b: float) -> float:
     return a + b
