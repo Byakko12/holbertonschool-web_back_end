7131e30b8bcc5a28da619aafb490fa5c201c87b1
Byakko12 <davidlandg@gmail.com>
2022-09-19 22:40:18 -0500

    [FIX] - comments of function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""
-add - takes a float a and a float b as arguments and
-returns their sum as a float.
-"""
+"""add - function"""
 
 
 def add(a: float, b: float) -> float:
+    """
+    takes a float a and a float b as arguments and
+    returns their sum as a float.
+    """
     return a + b
