f1e6e0b2f862972d0d849e4b079d082d5305c348
Byakko12 <davidlandg@gmail.com>
2022-09-20 23:26:06 -0500

    [FIX] - set to 10

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     Take the code from wait_n and alter it into a
      new function task_wait_n. The code is nearly identical
