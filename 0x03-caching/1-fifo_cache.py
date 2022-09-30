dda3f3f724c52716b58981a786033c13fc0fcf72
Byakko12 <davidlandg@gmail.com>
2022-09-29 19:25:07 -0500

    [FIX] - statments

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
-""" Python caching systems """
-
-from base_caching import BaseCaching
+"""
+FIFO caching
+"""
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFO caching system """
+    """
+    class FIFOCache that inherits from BaseCaching and is a caching system
+    """
 
-    def __init__(self):
-        ''' Initialize class instance. '''
+    def __init__(self) -> None:
+        """Initialize class instance"""
         super().__init__()
-        self.current_keys = []
+        self.temp_cache = []
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        """
+        Must assign to the dictionary self.cache_data for value key key
+        """
         if key is not None or item is not None:
             self.cache_data[key] = item
-            if key not in self.current_keys:
-                self.current_keys.append(key)
-            if len(self.current_keys) > BaseCaching.MAX_ITEMS:
-                discarded_key = self.current_keys.pop(0)
-                del self.cache_data[discarded_key]
-                print('DISCARD: {}'.format(discarded_key))
+        if key not in self.temp_cache:
+            self.temp_cache.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded = self.temp_cache.pop(0)
+            del self.cache_data[discarded]
+            print("DISCARD: {}".format(discarded))
 
     def get(self, key):
-        """ Get an item by key """
+        """
+        Must return the value in self.cache_data linked to key.
+        """
         return self.cache_data.get(key)
