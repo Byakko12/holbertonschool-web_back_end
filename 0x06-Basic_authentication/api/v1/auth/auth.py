ea9f8afad5ca5d6f54d93c18b23836ccc9f7fa48
Byakko12 <davidlandg@gmail.com>
2022-10-06 18:31:10 -0500

    [ADD] - task N4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module of Auth"""
+from queue import Empty
 from typing import TypeVar, List
 from flask import request
 
@@ -12,7 +13,22 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require function"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        for paths in excluded_paths:
+            if paths.endswith('*'):
+                if path.startswith(paths[:-1]):
+                    return False
+            elif path == paths:
+                return False
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header"""
