a7aaedb7335cfbd8e035d5082559a79e25367381
Byakko12 <davidlandg@gmail.com>
2022-10-06 12:51:17 -0500

    [ADD] - files

--- 0x06-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x06-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def raise_401() -> None:
+    """ GET /api/v1/unauthorized
+   Abort:
+      - Raises 401 error to test response
+    """
+    abort(401)
