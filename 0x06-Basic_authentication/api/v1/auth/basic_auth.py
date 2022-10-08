73263d541c60429f806680a23e683e7263008040
Byakko12 <davidlandg@gmail.com>
2022-10-07 19:11:43 -0500

    [ADD] - file for task N8

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """BasicAuth Model"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -17,3 +20,17 @@ class BasicAuth(Auth):
             return None
         base = authorization_header.split(' ')
         return base[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Returns decoded value of Base64 str """
+
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
