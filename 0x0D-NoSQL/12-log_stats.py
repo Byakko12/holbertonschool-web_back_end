088a20530036af157836671e78ef66b891bf9d71
Byakko12 <davidlandg@gmail.com>
2022-11-16 23:07:41 -0500

    [FIX]

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
-"""Prints statistics from the nginx collection"""
+"""Log stats"""
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def helper(a: dict) -> int:
+    """return log"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
+    return logs.count_documents(a)
 
-    print(logs.count_documents({}), 'logs')
-    print('Methods:')
-    method = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for req in method:
-        print(f'\tmethod {req}:', logs.count_documents({'method': req}))
-    print(logs.count_documents({'method': 'GET','path': '/status'}), 'status check')
+
+def main():
+    """ provides some stats about Nginx logs stored in MongoDB """
+    print(f"{helper({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {helper({'method': 'GET'})}")
+    print(f"\tmethod POST: {helper({'method': 'POST'})}")
+    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
+    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    main()
