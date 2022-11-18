8ac3ae0c7a652d1b3e4807f93c1068ec21e06edf
Byakko12 <davidlandg@gmail.com>
2022-11-17 23:40:43 -0500

    [FIX] - Identation of files

--- 0x10-ES6_classes/10-car.js
+++ 0x10-ES6_classes/10-car.js
@@ -1,16 +1,16 @@
 export default class Car {
     constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
     }
-  
+
     static get [Symbol.species]() {
-      return this;
+        return this;
     }
-  
+
     cloneCar() {
-      const Species = this.constructor[Symbol.species];
-      return new Species(this._brand, this._motor, this._color);
+        const Species = this.constructor[Symbol.species];
+        return new Species(this._brand, this._motor, this._color);
     }
-  }
+}
--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 export default class Airport {
     constructor(name, code) {
-      this._name = name;
-      this._code = code;
+        this._name = name;
+        this._code = code;
     }
-  
+
     get [Symbol.toStringTag]() {
-      return this._code;
+        return this._code;
     }
-  }
+}
