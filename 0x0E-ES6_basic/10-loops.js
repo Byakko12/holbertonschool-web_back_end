4dd3fbda3ab4591bcfcb1fef2ca4609e9316915c
Byakko <byakko@Xubuntu.myguest.virtualbox.org>
2022-12-09 22:25:18 -0500

    [ADD] - files for ES6_BASIC

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const copyArray = array;
+  for (const idx of copyArray) {
+    const value = copyArray[idx];
+    copyArray[idx] = appendString + value;
+  }
+
+  return copyArray;
+}
